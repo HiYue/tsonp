19bee9769154b987b54f198dff5ca04c11cff013
Justin Wang <justinwang24@yahoo.com.au>
2019-04-05 15:06:09 +1100

    Init

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+/**
+ * To implement jsonp in TypeScript
+ * Features:
+ *  - debounce: use lodash _debounce() function to add debounce ability
+ */
---
+++ tests/main.spec.ts
@@ -0,0 +1,6 @@
+// import {jsonp} from '../src/index';
+
+
+test('should return true given internal link', () => {
+    expect(1 === 1).toBe(true)
+  })
